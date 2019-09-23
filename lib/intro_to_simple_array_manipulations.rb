d3806f98d4a131b9b06bbc7574ec5b2409bb3a1b
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-09-23 14:44:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def tv_shows = ["Friends","Big Bang Theory","Two And A Half Men"]
+  another_show = "Family Guy"
+  tv_shows.push(another_show)
+end
+
+def tv_shows = ["Friends","Big Bang Theory","Two And A Half Men"]
+  tv_shows.unshift("Gilligand's Island")
+end
+
+def tv_shows = ["Friends","Big Bang Theory","Two And A Half Men"]
+  two_and_a_half_men = tv_shows.pop
+end
+
+
+  
